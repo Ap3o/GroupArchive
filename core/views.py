b70d127803eb2382d715c109e61ff7316018bfdb
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-12-02 22:47:41 +0500

    text about

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from . import models
 # Create your views here.
 def main(request):
     gallery = models.Gallery.objects.all()
-    return render(request, "main.html", {"gallery": gallery})
+    return render(request, "main.html", {"gallery": gallery, "text": "Все фотографии, которые только есть на сайте"})
 
 
 def delete_photo(request):
